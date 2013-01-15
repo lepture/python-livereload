5f85f66330e807ccb89387ec3ad15beabfd40d8f
Hsiaoming Yang <lepture@me.com>
2013-01-15 20:43:24 +0800

    options fix for #16

--- livereload/cli.py
+++ livereload/cli.py
@@ -1,28 +1,27 @@
 #!/usr/bin/env python
 
 from docopt import docopt
-from livereload import server
+from .server import start
 
 
 cmd = """Python LiveReload
 
 Usage:
-    livereload [-p <port>|--port=<port>] [-d <directory>|--directory=<directory>]
+    livereload [-p <port>|--port=<port>] [<directory>]
 
 Options:
     -h --help                       show this screen
     -p <port> --port=<port>         specify a server port, default is 35729
-    -d <root> --directory=<root>    specify a web root, default is '.'
 """
 
 
 def main():
     args = docopt(cmd)
     port = args.get('--port')
-    root = args.get('--directory')
+    root = args.get('<directory>')
     if port:
         port = int(port)
     else:
         port = 35729
 
-    server.start(port, root)
+    start(port, root)
