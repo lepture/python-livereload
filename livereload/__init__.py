bc2c698fb3e80288c81f72245dfb8553167783ac
Hsiaoming Yang <me@lepture.com>
2013-05-09 15:10:56 +0800

    release 1.0.0

--- livereload/__init__.py
+++ livereload/__init__.py
@@ -133,6 +133,6 @@ If you are on a Mac, you can buy `LiveReload2 <http://livereload.com/>`_.
 If you are a rubist, you can get guard-livereload.
 """
 
-__version__ = '0.15'
-__author__ = 'Hsiaoming Yang <lepture@me.com>'
+__version__ = '1.0.0'
+__author__ = 'Hsiaoming Yang <me@lepture.com>'
 __homepage__ = 'http://lab.lepture.com/livereload/'
