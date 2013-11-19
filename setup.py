816acabbd9e05dafdbb09cf932ede6d0d14fdbb6
Hsiaoming Yang <me@lepture.com>
2013-11-19 10:35:03 +0800

    Use scripts instead of entry_points.

--- setup.py
+++ setup.py
@@ -1,18 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
-ROOT = os.path.dirname(__file__)
-
-import sys
-kwargs = {}
-kwargs['include_package_data'] = True
-major, minor = sys.version_info[:2]
-if major >= 3:
-    kwargs['use_2to3'] = True
-
-from setuptools import setup, find_packages
 import livereload
+from setuptools import setup
 from email.utils import parseaddr
 author, author_email = parseaddr(livereload.__author__)
 
@@ -22,16 +12,17 @@ setup(
     author=author,
     author_email=author_email,
     url=livereload.__homepage__,
-    packages=find_packages(),
+    packages=['livereload'],
     description='Python LiveReload is an awesome tool for web developers',
     long_description=livereload.__doc__,
-    entry_points={
-        'console_scripts': ['livereload= livereload.cli:main'],
-    },
+    scripts=[
+        'bin/livereload',
+    ],
     install_requires=[
         'tornado', 'docopt',
     ],
-    license=open(os.path.join(ROOT, 'LICENSE')).read(),
+    license='BSD',
+    include_package_data=True,
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
@@ -43,11 +34,11 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Build Tools',
         'Topic :: Software Development :: Compilers',
         'Topic :: Software Development :: Debuggers',
-    ],
-    **kwargs
+    ]
 )
